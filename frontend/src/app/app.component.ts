182900c67155b0f7f40a48165d2bdcc31a494f81
Kalarrs Topham <k-dawg-klakr@hotmail.com>
2019-07-14 17:15:36 -0700

    Min version of node. Angular side reactive pattern.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,19 @@
 import {Component} from '@angular/core';
 import {webSocket} from 'rxjs/webSocket';
+import {merge, Observable, Subject} from 'rxjs';
+import {map, mapTo, scan, shareReplay, startWith} from 'rxjs/operators';
+import {FormBuilder, FormGroup} from '@angular/forms';
+
+
+interface DemoState {
+  command: string;
+  process: {
+    isRunning: boolean;
+    exitCode: 0 | 1;
+    stdOut: string;
+    command: string;
+  };
+}
 
 @Component({
   selector: 'app-root',
@@ -7,10 +21,41 @@ import {webSocket} from 'rxjs/webSocket';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'reactive-cli';
-  subject = webSocket('ws://localhost:3000');
+  commandFormGroup = this.fb.group({
+    command: [null, {updateOn: 'blur'}]
+  });
 
-  constructor() {
+  commandControl = this.commandFormGroup.get('command');
+
+  // subject = webSocket('ws://localhost:3000');
+
+  initialDemoState: DemoState = {
+    command: null,
+    process: null
+  };
+
+  commandChange$ = this.commandControl.valueChanges;
+
+  processChangeSubject = new Subject();
+  processChange$ = this.processChangeSubject.asObservable();
+
+  stateCommands$ = merge(
+    this.commandChange$.pipe(map((command) => ({command}))),
+    this.processChange$
+  );
+
+  state$: Observable<DemoState> = this.stateCommands$.pipe(
+    startWith(this.initialDemoState),
+    scan((currentDemoState: DemoState, command): DemoState => {
+      return {...currentDemoState, ...command};
+    }),
+    shareReplay(1)
+  );
+
+  constructor(private fb: FormBuilder) {
+
+    this.commandControl.valueChanges.subscribe(e => console.log(e));
+    /*
     console.log('WHAT?');
     this.subject.subscribe(res => console.log('Got a response!', res));
 
@@ -20,5 +65,31 @@ export class AppComponent {
     });
 
     // this.subject.complete();
+     */
+  }
+
+  execute(command: string) {
+    this.processChangeSubject.next({
+      process: {
+        id: 1,
+        isRunning: true,
+        exitCode: null,
+        command
+      }
+    });
+
+    setTimeout(() => {
+      this.processChangeSubject.next({
+        process: {
+          id: 1,
+          isRunning: false,
+          exitCode: 0,
+          command
+        }
+      });
+    }, 10000);
   }
 }
+
+// command ["cat file.txt", "curl xyz"]
+// process []
